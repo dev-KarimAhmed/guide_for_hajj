84ff22219e6cdae4c03d7cfe25e6a83b58809af1
Karim Ahmed <devKarimAhmed@gmail.com>
2025-02-08 13:33:52 +0200

    add assets (fonts & images)

---
+++ lib/guide_for_hajj_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GuideForHajjView extends StatelessWidget {
+  const GuideForHajjView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          children: [
+          
+          ],
+        ),
+      ),
+    );
+  }
+}
